08eae8662914a901a86fd7332441949b95b86e16
Linda Andersson <linda.29.andersson@gmail.com>
2023-08-01 11:37:04 +0200

    Color setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import * as d3 from "d3"; 
+import * as d3 from "d3";
 import axios from "axios";
 
 import "./App.css";
@@ -25,13 +25,25 @@ function App() {
   const root = d3.hierarchy(data).sum((d) => d.value);
   treemap(root);
 
+  // Define different fill colors
+  const colors = d3.scaleOrdinal(d3.schemeCategory10);
+
   return (
     <>
       <div id="title">Treemap Diagram Title</div>
       <div id="description">Description about the treemap</div>
       <svg width="800" height="500">
         {root.leaves().map((leaf, i) => (
-          <g key={`group-${i}`} transform={`translate(${leaf.x0},${leaf.y0})`}>
+          <g
+            key={`group-${i}`}
+            transform={`translate(${leaf.x0},${leaf.y0})`}
+            onMouseEnter={() => {
+              // Handle tooltip display here
+            }}
+            onMouseLeave={() => {
+              // Handle tooltip hide here
+            }}
+          >
             <rect
               className="tile"
               data-name={leaf.data.name}
@@ -39,14 +51,22 @@ function App() {
               data-value={leaf.data.value}
               width={leaf.x1 - leaf.x0}
               height={leaf.y1 - leaf.y0}
-              fill={`#FFFF`} 
+              fill={colors(i)}
             />
           </g>
         ))}
       </svg>
       <div id="legend">
         {/* Create the legend here with at least 2 different fill colors */}
-        {/* Customize as per your dataset and styling preference */}
+        {root.children.map((child, i) => (
+          <div key={`legend-item-${i}`} className="legend-item">
+            <span
+              className="legend-color"
+              style={{ background: colors(i) }}
+            ></span>
+            <span className="legend-label">{child.data.name}</span>
+          </div>
+        ))}
       </div>
       <div id="tooltip" />
     </>
